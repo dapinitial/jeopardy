9e8547e90149e8cfc9cd5d4b68931ab40aa3a89f
David Puerto <me@davidpuerto.com>
2017-12-22 15:39:53 -0800

    setting up the redux store and fetching from the API as I scaffold my application

---
+++ src/components/App/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  componentDidMount() {
+    fetch("http://jservice.io/api/categories?count=20")
+      .then(response => response.json())
+      .then(json => console.log(json));
+  }
+  render() {
+    return (
+      <div>
+        <h2>Jeopardy App!</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
