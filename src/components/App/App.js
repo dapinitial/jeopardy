4f7047277e611110a0bd7f66d2c479d3ce24e84c
David Puerto <me@davidpuerto.com>
2017-12-22 17:25:40 -0800

    mapping over categories

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,40 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { setCategories } from "../../actions";
+
+import "./App.css";
 
 class App extends Component {
   componentDidMount() {
-    fetch("http://jservice.io/api/categories?count=20")
-      .then(response => response.json())
-      .then(json => console.log(json));
+    if (this.props.categories.length === 0) {
+      fetch("http://jservice.io/api/categories?count=20")
+        .then(response => response.json())
+        .then(json => this.props.setCategories(json));
+    }
   }
+
   render() {
+    console.log("App props", this.props);
     return (
       <div>
         <h2>Jeopardy App!</h2>
+        {this.props.categories.map(category => {
+          return (
+            <div key={category.id}>
+              <Link to="/category">
+                <h4>{category.title}</h4>
+              </Link>
+            </div>
+          );
+        })}
       </div>
     );
   }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return { categories: state };
+}
+
+export default connect(mapStateToProps, { setCategories })(App);
--- src/index.js
+++ src/index.js
@@ -2,14 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import rootReducer from "./reducers";
 import App from "./components/App/App";
+import Category from "./components/Category/Category";
+
+import "./index.css";
 
 const store = createStore(rootReducer);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route exact path="/category" component={Category} />
+      </Switch>
+    </BrowserRouter>
   </Provider>,
   document.getElementById("root")
 );
